b5f3e8fa7869f8e2a1c6a21adbd3b2b23e709e05
George_Chakama <gtchakama@gmail.com>
2021-06-21 14:39:39 +0200

    axios | thoughts json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { Container, Button } from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,6 +25,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function App() {
   const classes = useStyles();
+
+  import axios from "axios";
+  axios.get("/thoughts.json").then((res) => {
+    console.log(res.data);
+  });
 
   return (
     <Container className={classes.root}>
