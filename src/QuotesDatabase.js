4d2b31caff373ebfa96548dcbd482a74aa43a8c9
George_Chakama <gtchakama@gmail.com>
2021-06-22 09:26:22 +0200

    Update Json

--- src/QuotesDatabase.js
+++ src/QuotesDatabase.js
@@ -2,12 +2,140 @@ const quotes = [
   {
     quote:
       "Theme parks can snap a crystal clear picture of you on a roller coaster at 70 mph, but bank cameras can't get a clear shot of a robber standing still.",
-    author: "Thomas Edison",
+    author: "Camera",
   },
   {
     quote:
       "If my calculator had a history, it would be more embarrassing than my browser history.",
-    author: "John Pierrakos",
+    author: "History",
+  },
+  {
+    quote:
+      "Lawyers hope you get sued, doctors hope you get sick, cops hope you're criminal, mechanics hope you have car trouble, but only a thief wishes prosperity for you.",
+    author: "Intentions",
+  },
+  {
+    quote:
+      "As a kid my parents taught me to not believe everything I see on TV, now I have to teach them to not believe everything they see on Facebook.",
+    author: "Being Naive",
+  },
+  {
+    quote:
+      "Tall people are expected to use their reach to help shorter people, but if a tall person were to ask a short person to hand them something they dropped on the floor it'd be insulting.",
+    author: "Height",
+  },
+  {
+    quote:
+      "Whoever created the tradition of not seeing the bride in the wedding dress beforehand saved countless husbands everywhere from hours of dress shopping and will forever be a hero to all men",
+    author: "Wedding",
+  },
+  {
+    quote:
+      "Being able to tolerate the sound of your own voice in a video is probably the highest form of self acceptance.",
+    author: "Voice",
+  },
+  {
+    quote:
+      "We do not check the refrigerator multiple times to find new food, we check to see if our standards have dropped enough to eat what was available.",
+
+    author: "Refrigerator",
+  },
+  {
+    quote:
+      "Apparently, a lemon is not naturally occurring and is a hybrid developed by cross breeding a bitter orange and a citron. Life never gave us lemons; we invented them all by ourselves.",
+    author: "Lemon",
+  },
+
+  {
+    quote:
+      "(Go to bed, you'll feel better in the morning) is the human version of (Did you turn it off and turn it back on again?)",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "What if Earth is like one of those uncontacted tribes in South America, like the whole Galaxy knows we're here but they've agreed not to contact us until we figure it out for ourselves.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "Instead of colorizing photos, in 50 years we will be removing filters.",
+    author: "Lemon",
+  },
+
+  {
+    quote: "I've woken up over 10,000 times and I'm still not used to it",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "Tobacco companies kill their best customers and condom companies kill their future customers.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "When a company offers me a better price after I cancel their subscription, they're just admitting they were overcharging me.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "Somewhere in the world, there is somebody with your dream job that hates going to work everyday.",
+    author: "Lemon",
+  },
+  {
+    quote: "Christmas feels more like a deadline than a holiday.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "(DO NOT TOUCH) would probably be a really unsettling thing to read in braille.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "Vehicles today can surf the web, link to your phone, stream music and videos, etc.. but they still can't perform a simple database lookup to tell you what the check engine light is on for.",
+    author: "Lemon",
+  },
+
+  {
+    quote:
+      "People who are goodlooking but have terrible personalities are basically real life click baits.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "When you drink alcohol you are just borrowing happiness from tomorrow.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "There should be a millenial edition of Monopoly where you just walk round the board paying rent, never able to buy anything.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "Gyms should have memberships where your fee goes down based on how often you go.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "My dog understand several human words. I don’t understand any dog barks. He may be smarter than me.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "I recognize click bait almost every time, but still want to know what that child celebrity looks like today.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "If Google matched people up by their browsing history, it could be the greatest online dating website of all time.",
+    author: "Lemon",
+  },
+  {
+    quote:
+      "If aliens come to earth, we have to explain why we made dozens of movies in which we fight and kill them.",
+    author: "Lemon",
   },
 ];
 export default quotes;
+// 39 - Thought
